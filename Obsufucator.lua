34a974c82a2dfc66660e42a416a71ea247987fe8
Dimanoname <gamefolover2019@gmail.com>
2022-12-12 14:47:59 +0200

    Upd

--- Obsufucator.lua
+++ Obsufucator.lua
@@ -1,10 +1,8 @@
 local thing = [[
-https://raw.githubusercontent.com/Dimanoname/2/main/Useless%20Gui.lua
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dimanoname/2/main/Useless%20Gui.lua"))()
 ]]
 
 
 local encoded = thing:gsub(".", function(bb) return "\\" .. bb:byte() end) or thing .. "\""
 
-print(encoded)
-print("Put the above encoded string into the loadstring below between the quotation marks for the obfuscated scrippts.")
-print'loadstring("")()'
+print("loadstring('" .. encoded .. "')()")
